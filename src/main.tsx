3d9d40374bf5884f9fe0ce05440c5a90b36cd308
Tyranteon <git@tyranteon.com>
2020-03-24 17:41:32 +0100

    first try

--- src/main.tsx
+++ src/main.tsx
@@ -1,124 +1,103 @@
-import { OtherComponent } from "@/other";
-import { BehaviorSubject, Subject } from "rxjs";
-import * as rxjs from "rxjs/operators";
-import { isArray } from "util";
+type SubscriptionFn<T> = (v: T) => void;
+type DestructionFn = () => void;
 
-export interface VNode {
-  tag: string | ComponentFn;
-  params: Record<string, any> | null;
-  children: PhxNode[];
-}
+class Reactive<T> {
+  value: T;
+  private readonly subscribers: SubscriptionFn<T>[];
 
-export type TextNode = string | number;
-
-export type PhxNode = VNode | TextNode | Subject<any>;
-
-export type ComponentFn = (props?: any) => PhxNode;
-
-export function phxCreateElement(
-  tag: string,
-  params: Record<string, any>,
-  ...children: VNode[]
-): VNode {
-  return {
-    tag,
-    params,
-    children
-  };
-}
-
-function AppComponent(): VNode {
-  const num$ = new BehaviorSubject(0);
-  const someVal$ = new BehaviorSubject("Hello");
-  const entries$ = new BehaviorSubject<Subject<string>[]>([]);
-
-  function add() {
-    num$.next(num$.value + 1);
-    entries$.value.push(new BehaviorSubject<string>("Element"));
-    entries$.next(entries$.value);
-    console.log("Adding");
+  constructor(value: T) {
+    this.value = value;
+    this.subscribers = [];
   }
 
-  function change(d: Subject<string>) {
-    d.next("Changed");
-  }
-
-  return (
-    <div>
-      <button onclick={add}>Add entry</button>
-      <OtherComponent stuff={someVal$} />
-      <div>Number of entries: {num$}</div>
-      {entries$.pipe(
-        rxjs.map(value =>
-          value.map((d, i) => (
-            <div>
-              {d} ({i + 1}/{num$})
-              <button onclick={() => change(d)}>Change</button>
-            </div>
-          ))
-        )
-      )}
-    </div>
-  );
-}
-
-function bootstrap() {
-  const v = AppComponent();
-  document.body.appendChild(buildNode(v));
-
-  console.log("App", v);
-}
-
-function buildNode(n: PhxNode): ChildNode | DocumentFragment {
-  if (typeof n === "string") {
-    return document.createTextNode(n);
-  }
-  if (typeof n === "number") {
-    return document.createTextNode(n.toString());
-  }
-
-  if (n instanceof Subject) {
-    const refNode = document.createComment("");
-    const fragment = document.createDocumentFragment();
-    fragment.appendChild(refNode);
-    let nodes: ChildNode[] = [];
-
-    n.subscribe(value => {
-      for (let node of nodes) {
-        node.remove();
-      }
-      nodes = [];
-      if (Array.isArray(value)) {
-        for (let e of value) {
-          nodes.push(buildNode(e) as ChildNode);
-        }
-      } else {
-        nodes.push(buildNode(value) as ChildNode);
-      }
-      refNode.after(...nodes);
-    });
-    return fragment;
-  }
-
-  if (typeof n.tag !== "string") {
-    const subtree = n.tag(n.params);
-    return buildNode(subtree)
-  }
-  const el = document.createElement(n.tag);
-  for (let child of n.children) {
-    el.appendChild(buildNode(child));
-  }
-  if (n.params !== null) {
-    for (let [key, value] of Object.entries(n.params)) {
-      if (key == "onclick") {
-        el.addEventListener("click", value);
-      } else {
-        el.setAttribute(key, value);
-      }
+  set(v: T): void {
+    this.value = v;
+    for (const sub of this.subscribers) {
+      sub(v);
     }
   }
 
-  return el;
+  subscribe(fn: SubscriptionFn<T>): DestructionFn {
+    this.subscribers.push(fn);
+    fn(this.value);
+    return () => {
+      this.subscribers.splice(
+        this.subscribers.findIndex(v => v === fn),
+        1
+      );
+    };
+  }
+}
+
+function reactive<T>(value: T): Reactive<T> {
+  return new Reactive<T>(value);
+}
+
+function AppComponent() {
+  let numClicked$ = reactive(0);
+
+  const addButton = document.createElement("button");
+  addButton.addEventListener("click", () => {
+    numClicked$.set(numClicked$.value + 1);
+  });
+  addButton.appendChild(document.createTextNode("Add"));
+
+  const removeButton = document.createElement("button");
+  removeButton.addEventListener("click", () => {
+    if (numClicked$.value > 0) {
+      numClicked$.set(numClicked$.value - 1);
+    }
+  });
+  removeButton.appendChild(document.createTextNode("Remove"));
+
+  let p = numClicked$.value;
+  let elements: DestructionFn[] = [];
+  numClicked$.subscribe(v => {
+    if (v > p) {
+      for (let i = v; i > p; i--) {
+        elements.push(ToDoEntryComponent(document.body, numClicked$));
+      }
+    } else if (p > v) {
+      const elementsToRemove = elements.splice(v);
+      for (let fn of elementsToRemove) {
+        fn();
+      }
+    }
+    p = v;
+  });
+
+  document.body.appendChild(addButton);
+  document.body.appendChild(removeButton);
+}
+
+function ToDoEntryComponent(
+  parent: Node,
+  prop$: Reactive<number>
+): DestructionFn {
+  const div = document.createElement("div");
+  const text = document.createTextNode("Entry X");
+  div.appendChild(text);
+  parent.appendChild(div);
+
+  console.log("Component");
+  let p = -1;
+  const subs = prop$.subscribe(v => {
+    if (p == v) {
+      return;
+    }
+    text.data = `Entry X / ${v}`;
+    console.log("Changed");
+    p = v;
+  });
+
+  return () => {
+    subs();
+    div.remove();
+  };
+}
+
+function bootstrap() {
+  AppComponent();
 }
 
 bootstrap();
